7cb6f3236cc0a0efa091d4218cce6d9de9b30ccc
Abyou <emandaabyou@gmail.com>
2022-03-21 10:44:13 -0500

    Adding playerName variables to store the player name and devtools console.log () to display it

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+// This creatsa function named "fight"
+
+function fight() {
+  window.alert("The fight has began!");
+}
+// fight();
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This is a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10 + 10);
+console.log("Our robit's name is: " + playerName);
